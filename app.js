6b595bacd99ed4407d61df758d39848dac798d35
brandontor <53887678+brandontor@users.noreply.github.com>
2021-06-10 21:17:53 -0400

    reconceptualized project after realizing narutoAPI uses a different language

--- app.js
+++ app.js
@@ -1,30 +1,98 @@
-//create a document selectors for each eyeball - DONE!
-//create event listeners for the click - DONE!
-//make event listeners that change the opacity of the eyeball to reveal the card behind the eyeball - DONE!
-//Create a function that will hide the eye on click
-//Create a function that will create a card and append it to the already created card div
-//create an API call that calls the naruto API and displays information on that specific shinobi
-
 const eyeballs = document.querySelectorAll('.eyeball');
 const card = document.querySelectorAll('.card');
 const sharingan = document.querySelectorAll('.sharingan');
+const firstRow = document.querySelector('.firstRow')
+const secondRow = document.querySelector('.secondRow')
+const thirdRow = document.querySelector('.thirdRow')
 //DOM target variables ^   
 
+//API CALL
+const request = new XMLHttpRequest();
+request.withCredentials = true;
 
-eyeballs.forEach(addHideEye)
-function addHideEye(node) {
-    node.addEventListener('click', hideEye);
+request.onload = function () {
+    let data = JSON.parse(this.response)
+    let team = data.api.players.filter(startAndDepth);
+    console.log(team)
+    
+	if(this.status >= 200 && this.status < 400) {   
+        team.forEach(function(player){
+            const col = document.createElement('div');
+            col.classList.add('col', 'm4', 'eyeball')
+            
+            const img = document.createElement('img');
+            img.setAttribute('src', `./assets/${player.firstName}.png`)
+            img.classList.add('responsive-img', 'sharingan')
+
+            const card = document.createElement('div')
+            card.classList.add('card')
+
+            const cardImgContainer = document.createElement('div')
+            cardImgContainer.classList.add('card-image')
+
+            const cardImg = document.createElement('img')
+            cardImg.setAttribute('src', `./assets/${player.firstName}.png`)
+
+            const cardContent = document.createElement('div')
+            cardContent.classList.add('card-content')
+
+            const name = document.createElement('P')
+            name.innerText = `${player.firstName}` + ` ${player.lastName}`
+
+            const details = document.createElement('P')
+            details.innerHTML = `Country: ${player.country} <br> DOB: ${player.dateOfBirth} <br> Height(m): ${player.heightInMeters}<br> Pos: ${player.leagues.standard.pos}`
+
+            const returnButtonDiv = document.createElement('div')
+            returnButtonDiv.classList.add('card-action')
+
+            const returnButton = document.createElement('a')
+            returnButton.innerText = 'Return';
+    
+            col.appendChild(img)
+            col.appendChild(card)
+            card.appendChild(cardImgContainer)
+            cardImgContainer.appendChild(cardImg)
+            card.appendChild(cardContent)
+            cardContent.appendChild(name)
+            cardContent.appendChild(details)
+            cardContent.appendChild(returnButtonDiv)
+            returnButtonDiv.appendChild(returnButton)
+
+            if(player.firstName == 'Kyle' || player.firstName == 'Fred' || player.firstName == "OG") {
+                firstRow.appendChild(col)
+            } else if(player.firstName == 'Pascal' || player.firstName == 'Khem' || player.firstName == "Malachi"){
+                secondRow.appendChild(col)
+            } else {
+                thirdRow.appendChild(col)
+            }
+
+            col.addEventListener('click', hideEye);
+        })
+    } else {
+        console.log('SADGE')
+    }
+};
+
+request.open("GET", "https://api-nba-v1.p.rapidapi.com/players/teamId/38");
+request.setRequestHeader("x-rapidapi-key", "6e109401b7msh4bc8d2137d42004p190d40jsn10d0d7bee39c");
+request.setRequestHeader("x-rapidapi-host", "api-nba-v1.p.rapidapi.com");
+
+request.send();
+
+
+//lowry vanvleet OG pascal Birch flynn trent yuta boucher
+function startAndDepth(teamMates) {
+    if(teamMates.playerId == "327" || teamMates.playerId == "527" || teamMates.playerId == "732" || teamMates.playerId == "479" || teamMates.playerId == "739" || teamMates.playerId == "2587" || teamMates.playerId == "1058" || teamMates.playerId == "1040" || teamMates.playerId == "745") {
+        return teamMates
+    }
 }
-//Adding event listeners to each node in the nodelist of eyeballs^
 
 function hideEye(e) {
     if(e.target.classList.contains('sharingan')){
         e.target.style.opacity = 0;
         setTimeout(changeDisplayNone, 500, e.target)
-        createCard(e.target.nextElementSibling);
+        setTimeout(changeDisplayIB, 500, e.target.nextElementSibling)
     }
-
-    
 }
 //The function for hiding the eye^
 
@@ -43,25 +111,3 @@ function changeOpacityOn(e) {
 }
 
 
-function createCard(e) {
-    const newCard = document.createElement('div')
-    newCard.classList.add('card-content')
-
-    const newDescription = document.createElement('P')
-    newDescription.innerText = 'I am Uchiha Sasuke'
-
-    const returnButtonDiv = document.createElement('div')
-    returnButtonDiv.classList.add('card-action')
-
-    const returnButton = document.createElement('a')
-    returnButton.innerText = 'Return';
-
-    newCard.appendChild(newDescription)
-    returnButtonDiv.appendChild(returnButton)
-    
-    e.appendChild(newCard)
-    e.appendChild(returnButtonDiv)
-
-    setTimeout(changeDisplayIB, 500, e)
-}
-//The function for creating the card element which will also allocate the API data^
